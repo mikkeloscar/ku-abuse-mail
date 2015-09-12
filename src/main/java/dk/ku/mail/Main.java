c6cb457f0cc9898385840f3915724b62f06b6f9b
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2015-09-12 18:36:14 +0200

    Move all Exchange logic to HandleAbuse

--- src/main/java/dk/ku/mail/Main.java
+++ src/main/java/dk/ku/mail/Main.java
@@ -1,8 +1,5 @@
 package dk.ku.mail;
 
-import microsoft.exchange.webservices.data.core.ExchangeService;
-import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
-
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +11,6 @@ public class Main {
         System.out.print("Enter password: ");
         String password = reader.nextLine();
 
-		ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
         HandleAbuse abuse = new HandleAbuse(username + "@ku.dk", password, "abuse@adm.ku.dk");
 		try {
             abuse.connect();
